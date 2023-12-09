c664136cefe9d34f33ac92e14b32cea00923f3e0
Kaung Myat Kyaw <kaungmyatkyaw18199@gmail.com>
2023-12-09 16:15:36 +0630

    kaung

--- src/views/SalesList.vue
+++ src/views/SalesList.vue
@@ -13,8 +13,8 @@ const router = useRouter();
 
 const { bookings, loading } = storeToRefs(bookingStore);
 
-const search = ref("");
 const chooseType = ref([]);
+const showSearchModal = ref(false);
 
 const changePage = async (url) => {
   console.log(url);
@@ -33,6 +33,24 @@ const watchSystem = computed(() => {
   if (searchP.value != "" && searchP.value != undefined) {
     result.status = searchP.value;
   }
+  if (limit.value != "" && limit.value != undefined) {
+    result.limit = limit.value;
+  }
+  if (search.value != "" && search.value != undefined) {
+    result.crm_id = search.value;
+  }
+  if (customerName.value != "" && customerName.value != undefined) {
+    result.customer_name = customerName.value;
+  }
+  if (balanceDueDate.value != "" && balanceDueDate.value != undefined) {
+    result.balance_due_date = balanceDueDate.value;
+  }
+  if (saleDate.value != "" && saleDate.value != undefined) {
+    result.sale_date = saleDate.value;
+  }
+  if (searchA.value != "" && searchA.value != undefined) {
+    result.filter = searchA.value;
+  }
 
   console.log(result);
   return result;
@@ -40,6 +58,13 @@ const watchSystem = computed(() => {
 
 const bookingStatus = ref("");
 const searchP = ref("");
+const limit = ref(10);
+const search = ref("");
+const customerName = ref("");
+const balanceDueDate = ref("");
+const saleDate = ref("");
+const searchA = ref("");
+
 const searchPayment = [
   { id: 1, name: "all", value: "All" },
   { id: 2, name: "not_paid", value: "Not Paid" },
@@ -51,11 +76,38 @@ const bookingStatusArr = [
   { id: 2, name: "confirmed" },
   { id: 3, name: "declined" },
 ];
+const searchArray = [
+  { id: 1, name: "all" },
+  { id: 2, name: "current" },
+  { id: 3, name: "past" },
+];
 
 const clear = () => {
   searchP.value = "";
   bookingStatus.value = "";
+  limit.value = 10;
+  search.value = "";
+  customerName.value = "";
+  balanceDueDate.value = "";
+  saleDate.value = "";
+  searchA.value = "";
 };
+const searchStatus = computed(() => {
+  if (
+    searchP.value != "" ||
+    bookingStatus.value != "" ||
+    limit.value != 10 ||
+    search.value != "" ||
+    customerName.value != "" ||
+    balanceDueDate.value != "" ||
+    saleDate.value != "" ||
+    searchA.value != ""
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+});
 
 onMounted(async () => {
   await bookingStore.getListAction();
@@ -66,6 +118,24 @@ watch(searchP, async (newValue) => {
   await bookingStore.getListAction(watchSystem.value);
 });
 watch(bookingStatus, async (newValue) => {
+  await bookingStore.getListAction(watchSystem.value);
+});
+watch(search, async (newValue) => {
+  await bookingStore.getListAction(watchSystem.value);
+});
+watch(searchA, async (newValue) => {
+  await bookingStore.getListAction(watchSystem.value);
+});
+watch(customerName, async (newValue) => {
+  await bookingStore.getListAction(watchSystem.value);
+});
+watch(balanceDueDate, async (newValue) => {
+  await bookingStore.getListAction(watchSystem.value);
+});
+watch(saleDate, async (newValue) => {
+  await bookingStore.getListAction(watchSystem.value);
+});
+watch(limit, async (newValue) => {
   await bookingStore.getListAction(watchSystem.value);
 });
 </script>
@@ -98,29 +168,27 @@ watch(bookingStatus, async (newValue) => {
       </div>
       <div class="rounded-3xl flex justify-between items-center">
         <div class="flex py-1.5 gap-3">
-          <v-select
-            class="style-chooser bg-transparent rounded-full border border-main min-w-[100px]"
-            :options="searchPayment"
-            v-model="searchP"
-            label="name"
-            :clearable="false"
-            :reduce="(d) => d.name"
-            placeholder="status"
-          ></v-select>
-          <v-select
-            class="style-chooser bg-transparent rounded-full border border-main min-w-[120px]"
-            :options="bookingStatusArr"
-            v-model="bookingStatus"
-            label="name"
-            :clearable="false"
-            :reduce="(d) => d.name"
-            placeholder="Booking"
-          ></v-select>
+          <div>
+            <input
+              v-model="search"
+              type="text"
+              class="bg-transparent rounded-full border border-main max-w-[150px] px-2 text-main py-2 text-sm"
+              placeholder="bookings id.."
+            />
+          </div>
+          <div>
+            <input
+              v-model="customerName"
+              type="text"
+              class="bg-transparent rounded-full border border-main max-w-[150px] px-2 text-main py-2 text-sm"
+              placeholder="Customer Name"
+            />
+          </div>
         </div>
-        <div class="mr-2" @click="clear">
+        <div class="mr-2 flex justify-end items-center gap-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            v-if="searchP == '' && bookingStatus == ''"
+            @click="showSearchModal = true"
             fill="none"
             viewBox="0 0 24 24"
             stroke-width="1.5"
@@ -134,7 +202,8 @@ watch(bookingStatus, async (newValue) => {
             />
           </svg>
           <img
-            v-if="searchP != '' || bookingStatus != ''"
+            @click="clear"
+            v-if="searchStatus"
             src="../../public/clear-svgrepo-com (1).svg"
             class="w-6 h-6"
             alt=""
@@ -144,6 +213,7 @@ watch(bookingStatus, async (newValue) => {
         <!-- @option:selected="chooseName()" -->
       </div>
     </div>
+
     <div
       class="relative flex justify-center items-center py-[50%]"
       v-if="loading"
@@ -178,6 +248,98 @@ watch(bookingStatus, async (newValue) => {
     </div>
     <div class="px-4">
       <Pagination v-if="!loading" :data="bookings" @change-page="changePage" />
+    </div>
+
+    <div
+      v-show="showSearchModal"
+      class="fixed inset-0 flex items-start h-full justify-center z-50"
+    >
+      <div
+        class="bg-white border-b border-black rounded-b-2xl px-8 pt-8 pb-2 transform animate__animated animate__fadeInDown w-full space-y-2"
+        enter-active-class="animate__animated animate__fadeInDown"
+        leave-active-class="animate__animated animate__fadeOutUp"
+      >
+        <div class="flex justify-between items-center">
+          <p class="text-lg font-semibold text-main">Search Sales List</p>
+          <p
+            @click="showSearchModal = false"
+            class="bg-main rounded-full px-2 py-1 text-sm text-white"
+          >
+            Close
+          </p>
+        </div>
+        <div class="flex py-1.5 flex-wrap gap-3">
+          <v-select
+            class="style-chooser bg-transparent rounded-full border border-main min-w-[100px]"
+            :options="searchPayment"
+            v-model="searchP"
+            label="name"
+            :clearable="false"
+            :reduce="(d) => d.name"
+            placeholder="status"
+          ></v-select>
+          <v-select
+            class="style-chooser bg-transparent rounded-full border border-main min-w-[120px]"
+            :options="bookingStatusArr"
+            v-model="bookingStatus"
+            label="name"
+            :clearable="false"
+            :reduce="(d) => d.name"
+            placeholder="Booking"
+          ></v-select>
+          <v-select
+            v-model="searchA"
+            class="style-chooser bg-transparent rounded-full border border-main min-w-[120px]"
+            :options="searchArray"
+            label="name"
+            :clearable="false"
+            :reduce="(d) => d.name"
+            placeholder="old pass ..."
+          ></v-select>
+          <div
+            class="flex items-center w-full justify-between gap-2 flex-nowrap"
+          >
+            <p class="text-sm text-main">Service Date</p>
+            <input
+              v-model="balanceDueDate"
+              type="date"
+              class="style-chooser bg-transparent rounded-full border border-main min-w-[120px] px-2 py-2 text-main text-sm"
+              placeholder="Search Date"
+              title="service_date"
+            />
+          </div>
+          <div
+            class="flex items-center w-full justify-between gap-2 flex-nowrap"
+          >
+            <p class="text-sm text-main">Sale Date</p>
+            <input
+              v-model="saleDate"
+              type="date"
+              class="style-chooser bg-transparent rounded-full border border-main min-w-[120px] px-2 py-2 text-main text-sm"
+              placeholder="Search Date"
+              title="sale_date"
+            />
+          </div>
+
+          <div
+            class="w-full flex justify-between border-t border-main pt-4 items-center"
+          >
+            <p class="inline-block mr-2 font-medium text-main">
+              Show Limit by One Page
+            </p>
+            <select
+              v-model="limit"
+              class="w-16 h-auto border-main p-2 border rounded-md focus:outline-none focus:ring-0"
+            >
+              <option value="10">10</option>
+              <option value="20">20</option>
+              <option value="30">30</option>
+              <option value="40">40</option>
+              <option value="50">50</option>
+            </select>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
