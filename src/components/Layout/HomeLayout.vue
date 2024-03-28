9c65758a19ebb55682abcc078e30f6a308687fc0
Kaung Myat Kyaw <kaungmyatkyaw18199@gmail.com>
2024-03-28 14:13:06 +0630

    kaung

--- src/components/Layout/HomeLayout.vue
+++ src/components/Layout/HomeLayout.vue
@@ -1,59 +1,64 @@
 <script setup>
 import { useRoute } from "vue-router";
+import { ref, computed } from "vue";
 const route = useRoute();
+
+// const currentUsage = ref(false);
 </script>
 
 <template>
-  <div>
-    <div
-      class="flex justify-start items-center text-xs font-semibold gap-3 px-4 py-2 overflow-x-scroll scroll-container"
-    >
-      <router-link
-        to="/"
-        class="cursor-pointer group hover:text-main whitespace-nowrap text-black/60 p-3 rounded-full"
-        :class="route.path == '/' ? 'text-main' : ''"
-      >
-        Van Tours
-        <p
-          :class="route.path == '/' ? 'block' : 'hidden'"
-          class="group-hover:block w-1 h-1 mx-auto mt-1 bg-main rounded-full"
-        ></p>
-      </router-link>
-      <router-link
-        to="/product/hotel"
-        class="cursor-pointer group hover:text-main whitespace-nowrap text-black/60 p-3 rounded-full"
-        :class="route.path == '/product/hotel' ? 'text-main' : ''"
-      >
-        Hotels
-        <p
-          :class="route.path == '/product/hotel' ? 'block' : 'hidden'"
-          class="group-hover:block w-1 h-1 mx-auto mt-1 bg-main rounded-full"
-        ></p>
-      </router-link>
-      <div
-        class="cursor-pointer group hover:text-main whitespace-nowrap text-black/60 p-3 rounded-full"
-      >
-        Attractions
-      </div>
-
-      <router-link
-        to="/product/van-tour"
-        active-class="bg-black/5"
-        class="cursor-pointer hover:text-main whitespace-nowrap text-black/60 p-3 rounded-full"
-      >
-        Hassle Free Tours
-      </router-link>
-
-      <router-link
-        to="/product/grouptour"
-        active-class="bg-black/5"
-        class="cursor-pointer hover:text-main whitespace-nowrap text-black/60 p-3 rounded-full"
-      >
-        Group Tours
-      </router-link>
-    </div>
+  <div class="relative">
     <div>
-      <slot />
+      <div
+        class="flex justify-start items-center text-xs font-semibold gap-3 px-4 py-2 overflow-x-scroll scroll-container"
+      >
+        <router-link
+          to="/"
+          class="cursor-pointer group hover:text-main whitespace-nowrap text-black/60 p-3 rounded-full"
+          :class="route.path == '/' ? 'text-main' : ''"
+        >
+          Van Tours
+          <p
+            :class="route.path == '/' ? 'block' : 'hidden'"
+            class="group-hover:block w-1 h-1 mx-auto mt-1 bg-main rounded-full"
+          ></p>
+        </router-link>
+        <router-link
+          to="/product/hotel"
+          class="cursor-pointer group hover:text-main whitespace-nowrap text-black/60 p-3 rounded-full"
+          :class="route.path == '/product/hotel' ? 'text-main' : ''"
+        >
+          Hotels
+          <p
+            :class="route.path == '/product/hotel' ? 'block' : 'hidden'"
+            class="group-hover:block w-1 h-1 mx-auto mt-1 bg-main rounded-full"
+          ></p>
+        </router-link>
+        <div
+          class="cursor-pointer group hover:text-main whitespace-nowrap text-black/60 p-3 rounded-full"
+        >
+          Attractions
+        </div>
+
+        <router-link
+          to="/product/van-tour"
+          active-class="bg-black/5"
+          class="cursor-pointer hover:text-main whitespace-nowrap text-black/60 p-3 rounded-full"
+        >
+          Hassle Free Tours
+        </router-link>
+
+        <router-link
+          to="/product/grouptour"
+          active-class="bg-black/5"
+          class="cursor-pointer hover:text-main whitespace-nowrap text-black/60 p-3 rounded-full"
+        >
+          Group Tours
+        </router-link>
+      </div>
+      <div>
+        <slot />
+      </div>
     </div>
   </div>
 </template>
--- src/components/Layout/Layout.vue
+++ src/components/Layout/Layout.vue
@@ -1,15 +1,35 @@
 <script setup>
 import NavbarVue from "./Navbar.vue";
 import FooterVue from "./Footer.vue";
+import { computed } from "vue";
+
+const currentUserUsage = computed(() => {
+  if (window.innerWidth > 768) {
+    return false;
+  } else {
+    return true;
+  }
+});
 </script>
 
 <template>
   <div class="text-sm relative">
-    <NavbarVue />
-    <div class="">
-      <slot />
+    <div
+      class="flex justify-center items-center w-full h-full"
+      v-if="!currentUserUsage"
+    >
+      <img
+        src="https://img.freepik.com/free-vector/eye-catching-coming-soon-web-page-background-with-megaphone-design_1017-50903.jpg?t=st=1711611402~exp=1711615002~hmac=95ca4b567f4b43e786a84e08214bdf819c1c539d42c7d2e9a4b6ca4cd7bb43ec&w=740"
+        alt=""
+      />
     </div>
+    <div v-if="currentUserUsage">
+      <NavbarVue />
+      <div class="">
+        <slot />
+      </div>
 
-    <FooterVue />
+      <FooterVue />
+    </div>
   </div>
 </template>
