ff4ed3d78977bc7306bb3996edbccd2346ebfb46
Kaung Myat Kyaw <kaungmyatkyaw18199@gmail.com>
2024-04-03 03:20:04 +0630

    kaung

--- src/router.js
+++ src/router.js
@@ -107,6 +107,15 @@ const routes = [{
 const router = createRouter({
     history: createWebHistory(),
     routes,
+    scrollBehavior(to, from, savedPosition) {
+        if (to.hash) {
+            // If the route has a hash, scroll to the element with that ID.
+            return { selector: to.hash };
+        } else {
+            // Scroll to the top of the page.
+            return { left: 0, top: 0 };
+        }
+    },
 });
 
 export default router;
