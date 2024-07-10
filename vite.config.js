6af7eaa44fae8c5be9f8d01cf3493a9cd9641dce
Kaung Myat Kyaw <kaungmyatkyaw18199@gmail.com>
2024-07-10 13:38:28 +0630

    kaung

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,55 @@
-import { fileURLToPath, URL } from "node:url";
+// import { fileURLToPath, URL } from "node:url";
 
+// import { defineConfig } from "vite";
+// import vue from "@vitejs/plugin-vue";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+//   assetsInclude: ["**/*.JPG"],
+//   resolve: {
+//     alias: {
+//       "@": fileURLToPath(new URL("./src", import.meta.url)),
+//     },
+//   },
+// });
+
+import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Thanywhere.com",
+        short_name: "Thanywhere.com",
+        theme_color: "#FF5B00",
+        background_color: "#FF5B00",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            src: "img/icons/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "img/icons/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        // Workbox options for more advanced caching strategies
+      },
+    }),
+  ],
   assetsInclude: ["**/*.JPG"],
   resolve: {
     alias: {
