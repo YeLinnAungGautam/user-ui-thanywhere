535d88b25e8a779ef7655194de59641e06a14d16
Kaung Kaung <kaungkaung@Kaungs-Laptop.local>
2024-09-17 19:21:50 +0700

    kaung

--- src/components/LoadingCarts/ImageCarouselForCart.vue
+++ src/components/LoadingCarts/ImageCarouselForCart.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="">
     <div class="relative overflow-hidden rounded-xl w-full h-full">
-      <carousel v-bind="settings">
+      <carousel v-bind="settings" @slide-start="handleSlideStart">
         <slide v-for="(slide, index) in showImage" :key="index">
           <div class="w-full h-full">
             <ImageCarouselCart :i="slide" :style="`w-full ${style}`" />
@@ -9,7 +9,38 @@
         </slide>
 
         <template #addons>
-          <pagination class="custom" />
+          <!-- <pagination class="custom" /> -->
+          <div
+            class="custom flex justify-start items-center w-[50px] space-x-1 overflow-hidden ml-auto pl-4"
+          >
+            <!-- <div
+              class="rounded-full"
+              v-for="(i, index) in showImage"
+              :key="i"
+              :class="[
+                currentIndex == index ? 'bg-white w-3 h-3' : 'bg-[#dfdadae9]',
+                index > 5 ? 'hidden' : '',
+                currentSlideIndex == index ? 'w-2.5 h-2.5' : 'w-1.5 h-1.5',
+                prevSlideIndex == index ? 'w-2.5 h-2.5' : 'w-1.5 h-1.5',
+                currentIndex < 3 &&  totalSlideCount < 6 ? index < 2 ? 'hidden': ''
+              ]"
+            ></div> -->
+            <div
+              v-for="(slide, index) in visibleDots"
+              :key="index"
+              :class="[
+                'rounded-full',
+                currentIndex === slide ? 'bg-white w-3 h-3' : 'bg-[#e2e2e2f9]',
+                currentIndex === slide ? 'w-[11px] h-[11px]' : 'w-1.5 h-1.5',
+                currentSlideIndex == slide
+                  ? 'w-2 h-2 bg-[#f2f2f2]'
+                  : 'w-1.5 h-1.5',
+                prevSlideIndex == slide
+                  ? 'w-2 h-2 bg-[#f2f2f2]'
+                  : 'w-1.5 h-1.5',
+              ]"
+            ></div>
+          </div>
         </template>
       </carousel>
     </div>
@@ -19,8 +50,8 @@
 <script setup>
 // import { ChevronDownIcon } from "@heroicons/vue/24/outline";
 import "vue3-carousel/dist/carousel.css";
-import { Carousel, Slide, Pagination } from "vue3-carousel";
-import { defineProps, ref, watch } from "vue";
+import { Carousel, Slide } from "vue3-carousel";
+import { defineProps, ref, watch, computed } from "vue";
 import ImageCarouselCart from "../LoadingCarts/ImageCarouselCartSec.vue";
 // import homeGradesdb from "../../assets/homeGrades";
 
@@ -28,6 +59,28 @@ const settings = {
   itemsToShow: 1,
   snapAlign: "start",
 };
+
+const currentIndex = ref(0);
+const totalSlideCount = ref(0);
+const currentSlideIndex = ref(0);
+const prevSlideIndex = ref(1);
+const handleSlideStart = (index) => {
+  console.log(index);
+  console.log("====================================");
+  console.log("====================================");
+  currentIndex.value = index.slidingToIndex;
+  totalSlideCount.value = index.slidesCount;
+  currentSlideIndex.value = index.slidingToIndex + 1;
+  prevSlideIndex.value = index.slidingToIndex - 1;
+};
+
+const visibleDots = computed(() => {
+  const maxVisibleDots = 6;
+  const start = Math.max(0, currentSlideIndex.value - 4);
+  const end = Math.min(start + maxVisibleDots, showImage.value.length);
+
+  return Array.from({ length: end - start }, (_, i) => start + i);
+});
 
 const props = defineProps({
   data: Array,
@@ -67,21 +120,5 @@ watch(
   position: absolute;
   bottom: 10px;
   width: 100%;
-}
-.carousel__pagination-button {
-  display: block;
-  border: 0;
-  margin: 0px 0px -7px 0px !important;
-  cursor: pointer;
-  padding: var(--vc-pgn-margin);
-  background: transparent;
-}
-.carousel__pagination {
-  display: flex;
-  justify-content: start !important;
-  list-style: none;
-  line-height: 0;
-  margin: 10px 0 0;
-  padding: 0;
 }
 </style>
--- src/components/hotelbookings/ImageCarousel.vue
+++ src/components/hotelbookings/ImageCarousel.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative overflow-hidden">
     <div class="relative">
-      <carousel v-bind="settings">
+      <carousel v-bind="settings" @slide-start="handleSlideStart">
         <slide v-for="(slide, index) in showImage" :key="index">
           <div class="w-full h-full">
             <!-- <div class="w-full h-[420px] overflow-hidden">
@@ -15,8 +15,42 @@
           </div>
         </slide>
 
-        <template #addons>
+        <!-- <template #addons>
           <pagination class="customPagi" />
+        </template> -->
+        <template #addons>
+          <!-- <pagination class="custom" /> -->
+          <div
+            class="custom flex justify-center items-center w-[50px] space-x-1 mb-8 overflow-hidden mx-auto"
+          >
+            <!-- <div
+              class="rounded-full"
+              v-for="(i, index) in showImage"
+              :key="i"
+              :class="[
+                currentIndex == index ? 'bg-white w-3 h-3' : 'bg-[#dfdadae9]',
+                index > 5 ? 'hidden' : '',
+                currentSlideIndex == index ? 'w-2.5 h-2.5' : 'w-1.5 h-1.5',
+                prevSlideIndex == index ? 'w-2.5 h-2.5' : 'w-1.5 h-1.5',
+                currentIndex < 3 &&  totalSlideCount < 6 ? index < 2 ? 'hidden': ''
+              ]"
+            ></div> -->
+            <div
+              v-for="(slide, index) in visibleDots"
+              :key="index"
+              :class="[
+                'rounded-full',
+                currentIndex === slide ? 'bg-white w-3 h-3' : 'bg-[#e2e2e2f9]',
+                currentIndex === slide ? 'w-[11px] h-[11px]' : 'w-1.5 h-1.5',
+                currentSlideIndex == slide
+                  ? 'w-2 h-2 bg-[#f2f2f2]'
+                  : 'w-1.5 h-1.5',
+                prevSlideIndex == slide
+                  ? 'w-2 h-2 bg-[#f2f2f2]'
+                  : 'w-1.5 h-1.5',
+              ]"
+            ></div>
+          </div>
         </template>
       </carousel>
     </div>
@@ -29,8 +63,8 @@
 <script setup>
 // import { ChevronDownIcon } from "@heroicons/vue/24/outline";
 import "vue3-carousel/dist/carousel.css";
-import { Carousel, Slide, Pagination } from "vue3-carousel";
-import { onMounted, defineProps, ref } from "vue";
+import { Carousel, Slide } from "vue3-carousel";
+import { onMounted, defineProps, ref, computed } from "vue";
 import ImageCarouselCart from "../LoadingCarts/ImageCarouselCart.vue";
 // import homeGradesdb from "../../assets/homeGrades";
 
@@ -39,11 +73,33 @@ const settings = {
   snapAlign: "start",
 };
 
+const currentIndex = ref(0);
+const totalSlideCount = ref(0);
+const currentSlideIndex = ref(0);
+const prevSlideIndex = ref(1);
+const handleSlideStart = (index) => {
+  console.log(index);
+  console.log("====================================");
+  console.log("====================================");
+  currentIndex.value = index.slidingToIndex;
+  totalSlideCount.value = index.slidesCount;
+  currentSlideIndex.value = index.slidingToIndex + 1;
+  prevSlideIndex.value = index.slidingToIndex - 1;
+};
+
 const props = defineProps({
   data: Array,
 });
 
-const showImage = ref(null);
+const visibleDots = computed(() => {
+  const maxVisibleDots = 6;
+  const start = Math.max(0, currentSlideIndex.value - 4);
+  const end = Math.min(start + maxVisibleDots, showImage.value.length);
+
+  return Array.from({ length: end - start }, (_, i) => start + i);
+});
+
+const showImage = ref([]);
 
 const simple = [
   {
