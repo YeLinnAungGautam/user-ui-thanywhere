1047593e68ea2f94744d9b95c03d7285dcbf13ad
Kaung Kaung <kaungkaung@Kaungs-Laptop.local>
2024-09-19 18:19:58 +0700

    kaung

--- src/views/WebHomePage.vue
+++ src/views/WebHomePage.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-[100vw] h-[100vh] grid grid-cols-2 overflow-hidden bg-gradient-to-t to-[#e67c1d] from-[#e61c1c] relative"
+    class="w-[100vw] h-[100vh] grid grid-cols-2 overflow-hidden bg-gradient-to-t to-[#ff6300] from-[#e61c1c] relative"
   >
     <!-- <img
       :src="line"
@@ -14,9 +14,9 @@
           THAILAND ANYWHERE
         </p>
       </div>
-      <div class="space-y-5">
+      <div class="space-y-5 pt-10">
         <p
-          class="text-white font-semibold tracking-normal pb-4 text-4xl flex justify-start items-center gap-x-2"
+          class="text-white font-semibold tracking-normal text-4xl flex justify-start items-center gap-x-2"
         >
           Only available on Mobile
           <ArrowRightCircleIcon class="w-10 text-white" />
@@ -24,25 +24,20 @@
         <p class="text-white text-3xl font-medium">Travel App for Thailand</p>
         <p class="text-white text-3xl font-medium">200+ packages</p>
       </div>
-      <div class="absolute bottom-20 space-y-6">
+      <div class="absolute bottom-20 space-y-3">
         <img :src="icon" alt="" class="w-40" />
-        <p class="text-[#ffbd38] font-medium tracking-normal text-3xl">
+        <p class="text-[#ffbd38] font-medium tracking-normal text-2xl">
           Coming soon on
         </p>
-        <p class="text-white text-3xl font-medium">Andriod and App stores</p>
+        <p class="text-white text-2xl font-medium">Andriod and App stores</p>
       </div>
     </div>
     <div class="relative">
       <!-- show mobile version with iframe -->
       <div class="flex justify-center items-center w-full h-full relative">
-        <img
-          :src="iphone"
-          alt=""
-          class="absolute w-[412px] h-[85vh] right-[21%] z-0"
-        />
         <iframe
           src="https://thanywhere.com"
-          class="w-[452px] h-full rounded-[55px] border-4 border-black/10 absolute right-[18%]"
+          class="w-[452px] h-[920px] rounded-[55px] border-4 border-black/5 shadow-3xl absolute right-[10%]"
           style="transform: scale(0.8); transform-origin: center"
           allowfullscreen
           sandbox="allow-same-origin allow-scripts"
@@ -55,7 +50,7 @@
 <script setup>
 import logo from "@/assets/logo.png";
 import icon from "@/assets/icon_download.png";
-import iphone from "@/assets/iphone.png";
+// import iphone from "@/assets/iphone.png";
 // import line from "@/assets/line.png";
 import { ArrowRightCircleIcon } from "@heroicons/vue/24/outline";
 </script>
