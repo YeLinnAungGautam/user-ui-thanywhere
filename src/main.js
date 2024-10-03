5daeeb89ff0e17646379e995bcacc973979c5953
Kaung Kaung <kaungkaung@Kaungs-Laptop.local>
2024-10-03 16:36:12 +0700

    kaung

--- src/main.js
+++ src/main.js
@@ -18,43 +18,43 @@ if (token) {
     axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 }
 
-// if ("serviceWorker" in navigator) {
-//     navigator.serviceWorker
-//         .register("/serviceworker.js", { scope: "/" })
-//         .then((registration) => {
-//             console.log("Service Worker registered with scope:", registration.scope);
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+        .register("/serviceworker.js", { scope: "/" })
+        .then((registration) => {
+            console.log("Service Worker registered with scope:", registration.scope);
 
-//             // Add update checking
-//             registration.addEventListener("updatefound", () => {
-//                 const newWorker = registration.installing;
-//                 newWorker.addEventListener("statechange", () => {
-//                     if (newWorker.state === "installed") {
-//                         if (navigator.serviceWorker.controller) {
-//                             showUpdateNotification();
-//                         }
-//                     }
-//                 });
-//             });
-//         })
-//         .catch((error) => {
-//             console.error("Service Worker registration failed:", error);
-//         });
-// }
+            // Add update checking
+            registration.addEventListener("updatefound", () => {
+                const newWorker = registration.installing;
+                newWorker.addEventListener("statechange", () => {
+                    if (newWorker.state === "installed") {
+                        if (navigator.serviceWorker.controller) {
+                            showUpdateNotification();
+                        }
+                    }
+                });
+            });
+        })
+        .catch((error) => {
+            console.error("Service Worker registration failed:", error);
+        });
+}
 
 // Add this function to show the update notification
-// function showUpdateNotification() {
-//     const toast = useToast();
-//     const message = "A new version is available. Click here to update.";
-//     toast.warning(message, {
-//         timeout: false,
-//         closeOnClick: false,
-//         draggable: false,
-//         closeButton: true,
-//         onClick: () => {
-//             window.location.reload();
-//         },
-//     });
-// }
+function showUpdateNotification() {
+    const toast = useToast();
+    const message = "A new version is available. Click here to update.";
+    toast.warning(message, {
+        timeout: false,
+        closeOnClick: false,
+        draggable: false,
+        closeButton: true,
+        onClick: () => {
+            window.location.reload();
+        },
+    });
+}
 
 const options = {
     timeout: 1500,
